3a3db3be49e47b9664bc1de03a8a2ab5399fdd06
Artur12121 <73768732+Artur12121@users.noreply.github.com>
2020-11-01 08:05:07 +0300

    Add files via upload

---
+++ layout/layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  layout
+//
+//  Created by iMAC on 30/10/2020.
+//  Copyright © 2020 iMAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController:
+    UIViewController
+{
+    @IBOutlet weak var textLable:
+    UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
